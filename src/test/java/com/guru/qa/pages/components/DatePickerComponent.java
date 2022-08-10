27dfef5043d78626306ebf30d6d07ecc75b73ef3
ronnika <lunkestr@gmail.com>
2022-08-10 23:30:32 +0300

    reformat datepicker

--- src/test/java/com/guru/qa/pages/components/DatePickerComponent.java
+++ src/test/java/com/guru/qa/pages/components/DatePickerComponent.java
@@ -5,15 +5,10 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DatePickerComponent {
-    private SelenideElement
-            dateOfBirthInput = $("#dateOfBirthInput"),
-            yearInput = $(".react-datepicker__year-select"),
-            monthInput = $(".react-datepicker__month-select");
-
     public DatePickerComponent setDate(String day, String month, String year) {
-        dateOfBirthInput.click();
-        yearInput.selectOptionByValue(year);
-        monthInput.selectOptionContainingText(month);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionByValue(year);
+        $(".react-datepicker__month-select").selectOptionContainingText(month);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
         return this;
     }
