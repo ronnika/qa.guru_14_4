fa880802269ef8c566404a14871fac84b022c16e
ronnika <lunkestr@gmail.com>
2022-08-31 23:49:07 +0300

    add attachments

--- src/test/java/com/guru/qa/tests/BaseTest.java
+++ src/test/java/com/guru/qa/tests/BaseTest.java
@@ -1,13 +1,35 @@
 package com.guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.guru.qa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
     @BeforeAll
     static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
